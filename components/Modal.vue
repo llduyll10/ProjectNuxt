4e3b286892c0b8902b58cab0a258cc833905b746
duynnd <>
2021-04-26 21:05:55 +0700

    init app

---
+++ components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <b-modal ref="my-modal" centered hide-footer title="">
+        <slot name="content"></slot>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:[],
+    methods: {
+      showModal() {
+        this.$refs['my-modal'].show()
+      },
+      hideModal() {
+        this.$refs['my-modal'].hide()
+      },
+      toggleModal() {
+        // We pass the ID of the button that we want to return focus to
+        // when the modal has hidden
+        this.$refs['my-modal'].toggle('#toggle-btn')
+      }
+    }
+  }
+</script>
+<style  lang="scss">
+.modal-header{
+    border-bottom: 0 !important;
+    padding-bottom: 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <button class="tn btn-danger" @click="openModal">Test bootstrap</button>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 content-left">
+
+          <Modal ref="modal">
+            <template v-slot:content >
+              <p>Content modal</p>
+            </template>
+          </Modal>
+        </div>
+        <div class="col-md-6">
+          Right
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modal from '@/components/Modal'
+export default {
+  components:{
+    Modal
+  },
+  methods:{
+    openModal(){
+      this.$refs.modal.showModal()
+    }
+  }
+};
+</script>
+<style lang="scss">
+@import "~/assets/scss/main.scss";
+.content-left {
+  height: 100%;
+}
+</style>
