df644c655910fb45c122401ac142816da811bd89
duynnd <>
2021-04-27 22:05:20 +0700

    update UI homepage

---
+++ components/Header.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="cover-header">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8 ct-left">
+                    <div class="d-flex">
+                        <img src="@/assets/img/black-logo.png"/>
+                        <div class="menu d-flex">
+                            <template v-for="(item, index) in objMenu">
+                                <div :key="index" class="sub-menu">
+                                   <span class="f-13">{{item.name}}</span>
+                                </div>
+                            </template>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 ct-right">
+                    <div class="d-flex">
+                        <div class="menu d-flex">
+                            <template v-for="(item, index) in objBtn">
+                                <div :key="index" class="sub-menu">
+                                   <span class="f-13">{{item.name}}</span>
+                                </div>
+                            </template>
+                            <div class="btn-findjob ml-4">
+                                <span class="f-13">Đăng Việc</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            objMenu:[
+                {name:'Thuê Nhà Thầu'},
+                {name:'Tìm Dự Án'},
+                {name:'Hướng Dẫn'},
+            ],
+            objBtn:[
+                {name:'Đăng Ký '},
+                {name:'Đăng Nhập '}
+            ]
+        }
+    }
+}
+</script>
+<style scoped lang="scss">
+.cover-header{
+    padding: 15px 65px;
+    .menu{
+        padding-top: 10px;
+        .sub-menu{
+            cursor: pointer;
+            width: 150px;
+            height: 25px;
+            margin-right: 10px;
+            text-align: center;
+            border-right:1px solid rgba(130, 130, 130, 0.5);
+            &:last-child{
+                border-right: 0;
+            }
+            span{
+                font-weight: bold;
+                line-height: 17px;
+                color: rgba(0, 0, 0, 0.84);
+            }
+        }
+    }
+    .ct-left{
+        img{
+            width: 130px;
+            height: 40px;
+        }
+        .menu{
+            margin-left: 50px;
+        }
+    }
+    .ct-right{
+        display: flex;
+        justify-content: flex-end;
+        .sub-menu{
+            width: 80px;
+            margin-right: 5px;
+            &:nth-child(2){
+                border-right: 0 !important;
+            }
+        }
+        .btn-findjob{
+            cursor: pointer;
+            width: 100px;
+            height: 30px;
+            padding: 2px 15px 0 16px;
+            text-align: center;
+            background: #198AC6;
+            color: #F2F2F2;
+            border-radius: 5px;
+            outline: none;
+        }
+    }
+}
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,197 @@
 <template>
-  <div id="homepage" class="container">
-    <button class="tn btn-danger" @click="openModal">Test bootstrap</button>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 content-left">
-          <Modal ref="modal">
-            <template v-slot:content>
-              <div class="modal-login">
-                <div class="text-center w-100">
-                  <img src="@/assets/img/black-logo.png"  style="height:52px"/>
+  <div id="homepage">
+    <Header />
+    <!-- Start Banner -->
+    <div class="banner"></div>
+    <!-- End Banner -->
+    <!-- Start Categories  -->
+    <div class="category mt-5">
+      <div class="category-title mb-4">
+        <p class="f-24">HẠNG MỤC DỰ ÁN</p>
+        <div class="line"></div>
+      </div>
+      <div class="container-fluid caterogry-list pd-45">
+        <div class="row">
+          <div class="col-md-4 col-sm-12 cat-item">
+              <div class="content item-1">
+                <div class="item-detail d-flex">
+                  <img src="@/assets/img/icon-house.png" style="margin-bottom:3px" />
+                  <p>
+                   THI CÔNG XÂY DỰNG
+                  </p>
                 </div>
-                <form>
-                  <div class="form-group mb-3 mt-5">
-                    <label class="f-13" for="exampleInputEmail1"
-                      >Email hoặc Số điện thoại <span style="color:red">*</span></label
-                    >
-                    <input
-                      type="email"
-                      class="form-control"
-                      aria-describedby="emailHelp"
-                      placeholder="Nhập email hoặc số điện thoại của bạn"
-                    />
-                  </div>
-                  <div class="w-100 text-center">
-                    <button class="btn btn-theme theme-blue">Đăng nhập</button>
-                  </div>
-                </form>
-                <div class="mt-4 w-100 option">
-                  <hr />
-                  <div
-                    class="child w-100 text-center">
-                    <span class="f-13">Hoặc</span>
-                  </div>
-                </div>
-                <div class="w-100 text-center mt-4">
-                  <button class="btn btn-theme theme-gg">
-                    Đăng nhập với google
-                    <img src="@/assets/img/gg.png" />
-                  </button>
-                </div>
-                <div class="w-100 text-center mt-4">
-                  <button class="btn btn-theme theme-fb">
-                    Đăng nhập với facebook
-                    <img src="@/assets/img/fb.png" style="width:28px" />
-                  </button>
-                </div>
-                <p class="text-center w-100 mt-4 f-13">
-                  Chưa có tài khoản?
-                  <span>Đăng ký ngay</span>
+              </div>
+          </div>
+          <div class="col-md-4 col-sm-12 cat-item">
+             <div class="content item-2">
+              <div class="item-detail d-flex">
+                <img src="@/assets/img/icon-decoration.png" />
+                <p>
+                  TRANG TRÍ NỘI THẤT
                 </p>
               </div>
-            </template>
-          </Modal>
-        </div>
-        <div class="col-md-6">
-          Right
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12 cat-item">
+            <div class="content item-3">
+              <div class="item-detail d-flex">
+                <img src="@/assets/img/icon-pen.png" />
+                <p>
+                  THIẾT KẾ 2D/3D
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
+    <!-- Start Categories  -->
+    <div class="describe mt-3">
+      <div class="content-describe">
+        <div class="row">
+          <!-- Describe LEFT -->
+          <div class="col-md-6 col-sm-12 describe-left">
+            <p class="slogan">
+              <span>Kết nối</span> bạn với nhà thầu <span>uy tín</span> và <span>chuyên nghiệp</span> nhất
+            </p>
+            <!-- Block 1 -->
+            <div class="block-content">
+              <div class="block-title d-flex">
+                <img src="@/assets/img/icon-chart.png" />
+                <p>Nhận ngay báo giá & hồ sơ từ nhiều đơn vị uy tín</p>
+              </div>
+              <div class="content">
+                <p>
+                  Chỉ cần bỏ ra <span> 5 phút</span> đăng yêu cầu dự án của bạn. Các nhà thầu của chúng tôi sẽ gửi ngay <span> hồ sơ năng lực, báo giá tạm tính </span> và <span> đề xuất giải pháp </span> ban đầu phù hợp cho công trình của bạn.
+                </p>
+              </div>
+            </div>
+            <!-- Block 2 -->
+            <div class="block-content">
+              <div class="block-title d-flex">
+                <img src="@/assets/img/icon-chart.png" />
+                <p>Nhận ngay báo giá & hồ sơ từ nhiều đơn vị uy tín</p>
+              </div>
+              <div class="content">
+                <p>
+                  Chỉ cần bỏ ra <span> 5 phút</span> đăng yêu cầu dự án của bạn. Các nhà thầu của chúng tôi sẽ gửi ngay <span> hồ sơ năng lực, báo giá tạm tính </span> và <span> đề xuất giải pháp </span> ban đầu phù hợp cho công trình của bạn.
+                </p>
+              </div>
+            </div>
+            <!-- Block 3 -->
+            <div class="block-content">
+              <div class="block-title d-flex">
+                <img src="@/assets/img/icon-chart.png" />
+                <p>Nhận ngay báo giá & hồ sơ từ nhiều đơn vị uy tín</p>
+              </div>
+              <div class="content">
+                <p>
+                  Chỉ cần bỏ ra <span> 5 phút</span> đăng yêu cầu dự án của bạn. Các nhà thầu của chúng tôi sẽ gửi ngay <span> hồ sơ năng lực, báo giá tạm tính </span> và <span> đề xuất giải pháp </span> ban đầu phù hợp cho công trình của bạn.
+                </p>
+              </div>
+            </div>
+            <!-- Block 4 -->
+            <div class="block-content">
+              <div class="block-title d-flex">
+                <img src="@/assets/img/icon-chart.png" />
+                <p>Nhận ngay báo giá & hồ sơ từ nhiều đơn vị uy tín</p>
+              </div>
+              <div class="content">
+                <p>
+                  Chỉ cần bỏ ra <span> 5 phút</span> đăng yêu cầu dự án của bạn. Các nhà thầu của chúng tôi sẽ gửi ngay <span> hồ sơ năng lực, báo giá tạm tính </span> và <span> đề xuất giải pháp </span> ban đầu phù hợp cho công trình của bạn.
+                </p>
+              </div>
+            </div>
+            <!-- Btn  -->
+            <div class="btn-registration mt-4 mb-2">
+              <span>ĐĂNG DỰ ÁN NGAY</span>
+            </div>
+          </div>
+          <!-- Describe RIGHT -->
+          <div class="col-md-6 col-sm-12 describe-right">
+            <div class="row">
+              <div class="col-12 mb-5">
+                <img src="@/assets/img/describe-1.png" />
+              </div>
+              <div class="col-12">
+                 <img src="@/assets/img/describe-2.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <Modal ref="modal">
+      <template v-slot:content>
+        <div class="modal-login">
+          <div class="text-center w-100">
+            <img src="@/assets/img/black-logo.png" style="height:52px" />
+          </div>
+          <form>
+            <div class="form-group mb-3 mt-5">
+              <label class="f-13" for="exampleInputEmail1"
+                >Email hoặc Số điện thoại
+                <span style="color:red">*</span></label
+              >
+              <input
+                type="email"
+                class="form-control"
+                aria-describedby="emailHelp"
+                placeholder="Nhập email hoặc số điện thoại của bạn"
+              />
+            </div>
+            <div class="w-100 text-center">
+              <button class="btn btn-theme theme-blue">Đăng nhập</button>
+            </div>
+          </form>
+          <div class="mt-4 w-100 option">
+            <hr />
+            <div class="child w-100 text-center">
+              <span class="f-13">Hoặc</span>
+            </div>
+          </div>
+          <div class="w-100 text-center mt-4">
+            <button class="btn btn-theme theme-gg">
+              Đăng nhập với google
+              <img src="@/assets/img/gg.png" />
+            </button>
+          </div>
+          <div class="w-100 text-center mt-4">
+            <button class="btn btn-theme theme-fb">
+              Đăng nhập với facebook
+              <img src="@/assets/img/fb.png" style="width:28px" />
+            </button>
+          </div>
+          <p class="text-center w-100 mt-4 f-13">
+            Chưa có tài khoản?
+            <span>Đăng ký ngay</span>
+          </p>
+        </div>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header';
 import Modal from "@/components/Modal";
 export default {
+  watch: {
+    "$route.query": "$fetch",
+  },
+  async asyncData(context) {},
+  async fetch() {
+    console.log("haha fetch", this.$store);
+  },
+  activated() {
+    if (this.$fetchState.timestamp <= Date.now() - 10000) {
+      this.$fetch();
+    }
+  },
   components: {
     Modal,
+    Header
   },
   methods: {
     openModal() {
