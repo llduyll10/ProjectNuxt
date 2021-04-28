5a54661b0ffc43f587c38ba6d081a0a5d66531ec
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-04-28 22:27:02 +0700

    update footer

---
+++ components/Footer.vue
@@ -0,0 +1,154 @@
+<template>
+    <footer id="footer" class="pt-60px">
+        <div class="container">
+            <div class="row links">
+                <div class="col-lg-3 col-md-3 link ">
+                    <img class="mb-12px" src="@/assets/img/Econslogobluewhite.png" alt="">
+                    <div class="wrap mt-15px">
+                        <div class="content f-13">
+                            Về chúng tôi
+                        </div>
+                        <div class="content f-13">
+                            Câu hỏi thường gặp
+                        </div>
+                        <div class="content f-13">
+                            Tin tức & nghề nghiệp
+                        </div>
+                        <div class="content f-13">
+                            Cơ hội nghề nghiệp
+                        </div>
+                        <div class="content f-13">
+                            Liên hệ
+                        </div>
+                        <div class="content f-13">
+                            Sitemap
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 link">
+                    <div class="title">
+                        <img class="user-icon" src="@/assets/img/footer-1.png">
+                        <div class="link f-14">
+                            HẠNG MỤC
+                        </div>
+                    </div>
+                    <div class="wrap mt-15px">
+                        <div class="content f-13">
+                            Thi công xây dựng
+                        </div>
+                        <div class="content f-13">
+                            Thi công nội thất
+                        </div>
+                        <div class="content f-13">
+                            Thiết kế kiến trúc
+                        </div>
+                        <div class="content f-13">
+                            Thiết kế nội thất
+                        </div>
+                        <div class="content f-13">
+                            Thiết kế đồ hoạ
+                        </div>
+                        <div class="content f-13">
+                            Tư vẫn giám sát
+                        </div>
+                        <div class="content f-13">
+                            Hạng mục khác
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 link">
+                    <div class="title">
+                        <img class="user-icon" src="@/assets/img/footer-2.png">
+                        <div class="link f-14">
+                            Hướng dẫn sử dụng
+                        </div>
+                    </div>
+                    <div class="wrap mt-15px">
+                        <div class="content f-13">
+                            Hướng dẫn xác thực tài khoản
+                        </div>
+                        <div class="content f-13">
+                        Hướng dẫn đăng dự án
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn hồ sơ và chào giá
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn quản lí dự án
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn kí hợp đồng
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn liên hệ trực tiếp
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn nhận tiền
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 link">
+                    <div class="title">
+                        <img class="user-icon" src="@/assets/img/footer-3.png">
+                        <div class="link f-14">
+                            Đăng kí chào giá
+                        </div>
+                    </div>
+                    <div class="wrap mt-15px">
+                        <div class="content f-13">
+                            Hướng dẫn tạo hồ sơ
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn mua Token
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn chào giá
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn xác thực tài khoản
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn kí hợp đồng
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn liên hệ khách hàng
+                        </div>
+                        <div class="content f-13">
+                            Hướng dẫn nhận tiền
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="line"></div>
+
+            <div class="row network">
+                <div class="col-lg-2 net-1">
+                    <img src="@/assets/svg/icon-user.svg" class="icon" alt="">
+                    <div class="phone">
+                        1-800-1234-567
+                    </div>
+                </div>
+                <div class="col-lg-4 net-2">
+                    <div class="contact">
+                        <!-- <img src="@/assets/svg/icon-face.svg" class="icon" alt=""> -->
+                        <img src="@/assets/svg/icon-p.svg" class="icon" alt="">
+                        <img src="@/assets/svg/icon-likedin.svg" class="icon" alt="">
+                        <img src="@/assets/svg/icon-google.svg" class="icon" alt="">
+                        <img src="@/assets/svg/icon-youtobe.svg" class="icon" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="d-flex justify-content-end">
+                        <span class="title f-14">Tải ứng dụng</span>
+                        <div class="download-icon">
+                            <img src="@/assets/svg/icon-apple.svg" class="icon" alt="">
+                            <img src="@/assets/svg/icon-robo.svg" class="icon" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="line"></div>
+        </div>
+    </footer>
+</template>
