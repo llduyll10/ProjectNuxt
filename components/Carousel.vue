7199a3fb4710542443a066981a01a56d9d8d3098
duynnd <>
2021-05-03 13:50:50 +0700

    update carousel

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -18,6 +18,7 @@
 <script>
 import VueSlickCarousel from 'vue-slick-carousel';
 import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+ import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default {
   components: { VueSlickCarousel },
