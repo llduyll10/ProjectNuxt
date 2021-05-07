bc2ea51d56924ee1c3edc353775cbc7ac5865a6d
duynnd <>
2021-05-07 15:21:04 +0700

    update

--- components/profile/Description.vue
+++ components/profile/Description.vue
@@ -43,6 +43,44 @@
 
         <div class="line"></div>
 
+        <div class="type-field">
+            <div class="group-title mb-10px">
+                <p class="title">Lĩnh vực</p>
+            </div>
+            <div class="content pl-20px">
+                <ul>
+                    <li>Thiết kế nội thất căn hộ </li>
+                    <li>Thiết kế nội thất căn hộ </li>
+                    <li>Thiết kế nội thất căn hộ </li>
+                    <li>Thiết kế nội thất căn hộ </li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="type-field">
+            <div class="group-title mb-10px">
+                <p class="title">Năng lực</p>
+            </div>
+            <p class="content f-13">
+                Thành lập từ năm 2013, Econs luôn dẫn đầu trong lĩnh vực thiết kế và thi công xây dựng các công trình lớn và nhỏ. Các công trình của chúng tôi trải dài từ Hà Nội, Huế, Thành phố Hố Chí Minh và các tỉnh lân cận miền Tây. Với đội ngũ thiết kế thi công chuyên nghiệp & lành nghề, chúng tôi luôn đảm bảo công tác tư vấn, thiết kế đạt chất lượng qua các tiêu chí mang tính sáng tạo, hiện đại, chính xác và tiết kiệm tối ưu chi phí hoàn thiện cho quyền lợi của Quý Khách Hàng. <br> <br> Econs không ngừng đào tạo và nâng cao chất lượng tay nghề đội ngũ kỹ sư & công nhân thi công, song song với việc trang bị những công cụ thi công hiện đại giúp thực hiện công trình trong thời gian ngắn nhất, với giá thành thấp nhất và chất lượng không thay đổi theo năm tháng. Với đội ngũ chúng tôi, mỗi công trình là một tác phẩm, một “đứa con” tinh thần được ấp ủ bởi niềm đam mê và lòng nhiệt huyết. Econs không ngừng đào tạo và nâng cao chất lượng tay nghề đội ngũ kỹ sư & công nhân thi công, song song với việc trang bị những công cụ thi công hiện đại giúp thực hiện công trình trong thời gian ngắn nhất, với giá thành thấp nhất và chất lượng không thay đổi theo năm tháng. Với đội ngũ chúng tôi, mỗi công trình là một tác phẩm, một “đứa con” tinh thần được ấp ủ bởi niềm đam mê và p ủ bởi niềm đam mê
+            </p>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="type-field">
+            <div class="group-title mb-10px">
+                <p class="title">Chứng chỉ năng lực</p>
+            </div>
+            <div class="group-certificate"></div>
+        </div>
+
+
+
+
+
     </section>
 </template>
 <script>
