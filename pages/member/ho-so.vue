f71ddc2ee0293bf8b6eb3e0bb62a30f484e70204
Alex Pham <phamyenbk@gmail.com>
2021-05-07 09:52:55 +0700

    update

---
+++ pages/member/ho-so.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <Header />
+        <section id="search-job" class="wrapSection pt-30px pb-80px">
+            <div class="wrapBanner hosoWrap"></div>
+            <div class="wrapCenter">
+              
+            </div>
+
+        </section>
+        <Footer />
+    </div>
+
+</template>
+
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer"
+import Calendar from "@/components/Calendar"
+export default {
+    // middleware: 'auth',
+    components:{
+        Header,
+        Footer,
+    },
+    data(){
+        return{
+            
+        }
+    },
+    mounted(){
+        
+    },
+    methods:{
+        
+    }
+}
+</script>
