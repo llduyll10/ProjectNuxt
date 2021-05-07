9faeee12abbbdf3e6bf55ad973ee1d18bea1146e
duynnd <>
2021-05-07 14:56:57 +0700

    up

--- components/profile/Description.vue
+++ components/profile/Description.vue
@@ -1,6 +1,44 @@
 <template>
-    <section class="profile-description">
-        ss
+    <section id="description-component">
+        <div class="title">
+            <p class="name">Long Bùi</p>
+            <div class="height"></div>
+            <p class="name-company">
+                Công Ty Cổ Phần Epoint
+                <img class="ml-10px" src="@/assets/svg/icon-verify-company.svg" alt="">
+            </p>
+        </div>
+        <div class="group-bage mt-20px">
+            <div class="cover-bage d-flex mb-15px">
+                <h5><span class="badge badge-secondary">Nội thất căn hộ</span></h5>
+                <h5><span class="badge badge-secondary">Nội thất căn hộ</span></h5>
+                <h5><span class="badge badge-secondary">Nội thất căn hộ</span></h5>
+            </div>
+            <div class="d-flex cover-type-location">
+                <div class="type">
+                    <img src="@/assets/svg/icon-user-dark.svg" />
+                    <span class="f-11 font-weight-bold">Công Ty Xây Dựng</span>
+                </div>
+                <div class="location">
+                    <img src="@/assets/svg/icon-location.svg" />
+                    <span class="f-11">Hồ Chí Minh</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="line"></div>
+
+        <div class="type-field">
+            <div class="group-title">
+                <p>Giới thiệu</p>
+                <div>
+                    <img src="@/assets/svg/icon-edit-file.svg" />
+                    <span>Chỉnh sửa</span>
+                    <span>Xoá bỏ</span>
+                </div>
+            </div>
+            <p>Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...</p>
+        </div>
     </section>
 </template>
 <script>
