23692050a8012c391a2a1483aef4a69aef9adcd2
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-05-08 23:20:15 +0700

    modal contact

--- components/popup/lien-he.vue
+++ components/popup/lien-he.vue
@@ -1,8 +1,32 @@
 <template>
-    <Modal ref="modalLien">
+    <Modal ref="modalLien" id="modal-custom-form-cotact">
       <template v-slot:content>
-        <div class="modal-login">
-           nkjksjkss
+        <div class="modal-contact">
+           <div class="top">
+                <div class="title f-20">
+                    Liên hệ trực tiếp với <span>Công ty cổ phần Epoint</span>
+                </div>
+                <div class="sub f-13">
+                    Bạn sẽ tốn <span>3 Tokens</span> khi liên hệ trực tiếp với <span>Công ty cổ phần Epoint</span>
+                </div>
+           </div>
+           <div class="center">
+               <div class="name-line">
+                   <div class="label">Họ Tên:</div>
+                   <div class="name">BÙI KIM LONG</div>
+               </div>
+               <div class="phone-line">
+                   <div class="label">Số điện thoại:</div>
+                   <div class="phone">090 465 9518</div>
+               </div>
+               <div class="email-line">
+                   <div class="label">Địa chỉ Email:</div>
+                   <div class="email">LONGB@EPOINT.VN</div>
+               </div>
+           </div>
+           <div class="bottom">
+               <div class="submit f-16">HOÀN TẤT</div>
+           </div>
         </div>
       </template>
     </Modal>
