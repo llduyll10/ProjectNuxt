06d29370cffc0d91d7cc16a06ea2be6364eaea9b
Thach Nguyen <nhoangthach.94@gmail.com>
2021-05-10 06:21:48 +0700

    Update chinh sua ho so

--- components/profile/EditProfile/editPersonalInfo.vue
+++ components/profile/EditProfile/editPersonalInfo.vue
@@ -23,20 +23,121 @@
                 </div>
             </div>
 
-            <div class="editRow">
+            <div class="editRow mb-15px">
                 <div class="fieldLabel">
-                    Người đại diện <span class="dot">*</span>
+                    Người đại diện<span class="dot">*</span>
                 </div>
                 <div class="fieldInput">
                     <div class="form-group mb-0">
                         <input
                             type="text"
-                            class="form-control col-md-9 col-sm-12"
+                            class="form-control"
                             required
                             placeholder="Họ và Tên"
                         />
                     </div>
                 </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Tên công ty<span class="dot">*</span>
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <input
+                            type="text"
+                            class="form-control"
+                            required
+                            placeholder="Tên công ty"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Số điện thoại<span class="dot">*</span>
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <input
+                            type="text"
+                            class="form-control"
+                            required
+                            placeholder="Số điện thoại"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Email<span class="dot">*</span>
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <input
+                            type="text"
+                            class="form-control"
+                            required
+                            placeholder="Email"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Địa chỉ<span class="dot">*</span>
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <input
+                            type="text"
+                            class="form-control"
+                            required
+                            placeholder="Họ và Tên"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Tỉnh thành<span class="dot">*</span>
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <select class="form-control">
+                            <option selected>Tỉnh thành</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="editRow mb-15px">
+                <div class="fieldLabel">
+                    Website tham khảo
+                </div>
+                <div class="fieldInput">
+                    <div class="form-group mb-0">
+                        <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Website/Link portfolio"
+                        />
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-100 my-40px">
+                <button type="button" class="btn btn-main w-100 btn-lg">
+                    CẬP NHẬT THÔNG TIN
+                </button>
             </div>
         </form>
     </div>
