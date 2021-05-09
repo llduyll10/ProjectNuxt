a7d82bbfd7bb2a02bf9f932b84d4bda34deba39b
Alex Pham <phamyenbk@gmail.com>
2021-05-09 07:42:42 +0700

    up

---
+++ components/popup/lien-he-form.vue
@@ -0,0 +1,22 @@
+<template>
+    <Modal ref="modalLienForm" id="modal-cotact-form-submit">
+      <template v-slot:content>
+        <div class="modal-contact formCore">
+          lam cai form o day
+        </div>
+      </template>
+    </Modal>
+</template>
+
+<script>
+export default {
+    methods:{
+        show() {
+            this.$refs.modalLienForm.showModal();
+        },
+        hide(){
+            this.$refs.modalLienForm.hideModal()
+        },
+    }
+}
+</script>
