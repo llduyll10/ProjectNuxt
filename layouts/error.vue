e578ed00b6a9a78e8b74ea623c92b3c3e87c88bf
Alex Pham <phamyenbk@gmail.com>
2021-05-09 15:51:34 +0700

    update error page

---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Header />
+        <div class="container-fluid">
+            <div class="text-center p-60px">
+                Không tìm thấy trang, <b-link :to="'/'">Quay lại trang chủ</b-link> 
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+    components:{
+        Header,
+        Footer
+    }
+}
+</script>
