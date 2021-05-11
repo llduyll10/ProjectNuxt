362c6dda29bfe0ec4cb3bb67b3aef66489b61cf0
duynnd <>
2021-05-11 16:11:51 +0700

    update

---
+++ components/profile/EditProfile/createEditProject.vue
@@ -0,0 +1,112 @@
+<template>
+     <div class="cover-edit-create">
+      <div v-if="objProject._id" class="f-20 f-bold text-main mb-24px">
+        Chỉnh sửa dự án
+      </div>
+      <div v-else class="f-20 f-bold text-main mb-24px">
+        Thêm dự án
+      </div>
+      <form @click.prevent="createProject">
+        <div class="editRow mb-15px">
+          <div class="fieldLabel font-weight-bold f-13">Tên dự án<span style="color:red">*</span></div>
+          <div class="fieldInput">
+            <div class="form-group mb-0">
+              <input
+                type="text"
+                class="form-control"
+                required
+                placeholder="Tên dự án bạn đã thực hiện"
+                v-model="objProject.nameProject"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="editRow mb-15px">
+          <div class="fieldLabel font-weight-bold f-13">Dịch vụ<span style="color:red">*</span></div>
+          <div class="fieldInput">
+            <div class=" mb-0">
+              <treeselect
+                :options="optionsCategory"
+                :disable-branch-nodes="true"
+                :value="objProject.category"
+                v-model="objProject.category"
+                :multiple="true"
+                placeholder="Select"
+            />
+            </div>
+          </div>
+        </div>
+
+        <div class="editRow mb-15px">
+          <div class="fieldLabel font-weight-bold f-13">Mô tả chi tiết<span style="color:red">*</span></div>
+          <div class="fieldInput">
+            <div class="form-group mb-0">
+              <textarea
+                class="form-control"
+                required
+                placeholder="Mô tả chi tiết dự án bạn đã thực hiện"
+                rows="8"
+                v-model="objProject.detailProject"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div class="editRow mb-15px">
+          <div class="fieldLabel font-weight-bold f-13">
+            Hình ảnh đính kèm
+          </div>
+          <div class="fieldInput">
+            <div>
+              <button type="button" class="btn btn-main f-11">
+                <img
+                  class="mr-4px h-13px"
+                  src="@/assets/svg/icon-upload.svg"
+                  alt=""
+                />
+                Thêm hình ảnh
+              </button>
+
+              <i class="text-main f-11">png, jpg, tiff</i>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-100 my-40px">
+          <div class="row">
+            <div class="col-6">
+              <button type="submit" class="btn btn-main w-100 btn-lg">
+                THÊM DỰ ÁN
+              </button>
+            </div>
+            <div class="col-6">
+              <button type="button" class="btn btn-main w-100 btn-lg bg-red">
+                HUỶ BỎ
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+</template>
+<script>
+export default {
+    props:['project'],
+    data(){
+        return{
+            objProject:{},
+            optionsCategory: this.getCategory()
+        }
+    },
+    mounted(){
+        this.objProject = {...this.project,...this.objProject}
+        console.log(this.objProject)
+    },
+    methods:{
+        createProject(){
+            console.log(this.objProject)
+        }
+    }
+}
+</script>
--- components/profile/EditProfile/editWorkProject.vue
+++ components/profile/EditProfile/editWorkProject.vue
@@ -13,7 +13,7 @@
                     <div class="d-flex justify-content-end mb-5px">
                         <div class="group-function">
                             <img src="@/assets/svg/icon-edit-file.svg" />
-                            <span>Chỉnh sửa</span>
+                            <span @click="openModal">Chỉnh sửa</span>
                             <div class="height"></div>
                             <span>Xoá bỏ</span>
                         </div>
@@ -34,98 +34,38 @@
             <p class="f-12 text-main">Xem thêm dự án <i class="fas fa-caret-down ml-5px"></i></p>
         </div>
     </div>
-    <div>
-      <div class="f-20 f-bold text-main mb-24px">
-        Thêm dự án
-      </div>
-      <form>
-        <div class="editRow mb-15px">
-          <div class="fieldLabel">Tên dự án<span class="dot">*</span></div>
-          <div class="fieldInput">
-            <div class="form-group mb-0">
-              <input
-                type="text"
-                class="form-control"
-                required
-                placeholder="Tên dự án bạn đã thực hiện"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div class="editRow mb-15px">
-          <div class="fieldLabel">Dịch vụ<span class="dot">*</span></div>
-          <div class="fieldInput">
-            <div class="form-group mb-0">
-              <select class="form-control">
-                <option selected>Thi công xây dựng phần thô</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div class="editRow mb-15px">
-          <div class="fieldLabel">Mô tả chi tiết<span class="dot">*</span></div>
-          <div class="fieldInput">
-            <div class="form-group mb-0">
-              <textarea
-                class="form-control"
-                required
-                placeholder="Mô tả chi tiết dự án bạn đã thực hiện"
-                rows="8"
-              />
-            </div>
-          </div>
-        </div>
-
-        <div class="editRow mb-15px">
-          <div class="fieldLabel">
-            Hình ảnh đính kèm
-          </div>
-          <div class="fieldInput">
-            <div>
-              <button type="button" class="btn btn-main f-11">
-                <img
-                  class="mr-4px h-13px"
-                  src="@/assets/svg/icon-upload.svg"
-                  alt=""
-                />
-                Thêm hình ảnh
-              </button>
-
-              <i class="text-main f-11">png, jpg, tiff</i>
-            </div>
-          </div>
-        </div>
-
-        <div class="w-100 my-40px">
-          <div class="row">
-            <div class="col-6">
-              <button type="button" class="btn btn-main w-100 btn-lg">
-                THÊM DỰ ÁN
-              </button>
-            </div>
-            <div class="col-6">
-              <button type="button" class="btn btn-main w-100 btn-lg bg-red">
-                HUỶ BỎ
-              </button>
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
+    <CreateEditProject :project=demoProject />
+    <Modal ref="modalEditProject">
+      <template v-slot:content>
+        <CreateEditProject :project=demoProject />
+      </template>
+    </Modal>
   </div>
 </template>
 <script>
 import DemoHouse from '@/assets/img/demo-house.png'
+import CreateEditProject from './createEditProject'
+import Modal from "@/components/Modal";
 export default {
+    components:{
+      CreateEditProject,
+      Modal
+    },
     data(){
         return{
-            demoHouse:DemoHouse
+            demoHouse:DemoHouse,
+            demoProject:{
+              name:'abc',
+            }
         }
+    },
+    methods:{
+      openModal(){
+        this.$refs.modalEditProject.showModal()
+      },
+      hideModal(){
+        this.$refs.modalEditProject.hideModal()
+      }
     }
 }
 </script>
