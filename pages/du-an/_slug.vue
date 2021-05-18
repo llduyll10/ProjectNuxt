835f5e01d247973c0deef5f70c11d3f08ed53d0f
Alex Pham <phamyenbk@gmail.com>
2021-05-19 06:57:48 +0700

    up

--- pages/du-an/_slug.vue
+++ pages/du-an/_slug.vue
@@ -69,8 +69,7 @@
               class="left inner-content-section px-36px pt-25px pb-50px mb-20px"
             >
               <h3 class="h5 main-black f-16 font-weight-bold">
-                Gửi chào giá cho dự án - Bạn sẽ tốn
-                <span class="main-color">3 Tokens</span> khi tham gia chào giá
+                Danh sách chào giá (3<span class="main-color">3</span> chào giá)
               </h3>
               <hr class="hr" />
               <template v-for="(id,index) in 3">
