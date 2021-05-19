22a578613f1ce9c770bc3a0c71ce7ef306f727ff
duynnd <>
2021-05-19 11:20:43 +0700

    update pagination

---
+++ components/PaginationCustom.vue
@@ -0,0 +1,31 @@
+<template>
+    <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        align="center"
+        @input="changePaging()"
+    >
+
+    </b-pagination>
+</template>
+<script>
+export default {
+    props:["count","perPage",],
+    data(){
+        return{
+            currentPage:1
+        }
+    },
+    computed:{
+        rows(){
+            return this.count
+        }
+    },
+    methods:{
+        changePaging(){
+            this.$emit('page',{page:this.currentPage,limit:this.perPage})
+        }
+    }
+}
+</script>
