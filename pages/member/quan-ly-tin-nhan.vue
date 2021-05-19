6d20bdd079652be1d9951962dcefc91880ede19f
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-05-20 04:49:53 +0700

    message

--- pages/member/quan-ly-tin-nhan.vue
+++ pages/member/quan-ly-tin-nhan.vue
@@ -73,9 +73,75 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-8">
-                        <div class="content-message">
-                            fsdfs
+                    <div class="col-lg-8 pl-5px">
+                        <div class="contentWrap">
+                            <div class="contentTop d-flex">
+                                <div class="title">Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa</div>
+                                <div class="wrapDate">
+                                   <img src="@/assets/svg/point-black.svg" alt="">
+                                   <div class="date ml-20px f-12">Đăng ngày  <span>20/04/2021</span></div>
+                                </div>
+                            </div>
+                            <div class="message">
+                                <div class="dateMessage">20/04/201 - 8:25 PM</div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                                <div class="dateMessage">20/04/201 - 8:25 PM</div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Econs nội thất.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Econs nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                                <div class="dateMessage">20/04/201 - 8:25 PM</div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Econs nội thất.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Econs nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Econs nội thất.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Econs nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Manager.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Ban nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                                <div class="dateMessage">20/04/201 - 8:25 PM</div>
+                                <div class="send d-flex">
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                    <div class="contentSend">Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất.</div>
+                                </div>
+                                <div class="receive d-flex">
+                                    <div class="contentReceive">Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất...Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất.</div>
+                                    <img src="@/assets/svg/messageLong.svg" alt="">
+                                </div>
+                            </div>
+                            <div class="wrapFill d-flex">
+                                <input type="text" placeholder="Soạn tin nhắn...">
+                                <img src="@/assets/svg/happy.svg" alt="">
+                                <img src="@/assets/svg/file.svg" alt="">
+                                <div class="sending">Gửi tin nhắn</div>
+                            </div>
                         </div>
                     </div>
                 </div>
