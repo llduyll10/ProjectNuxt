e064746a1100181755f7d47b10589c2aaaf9fb9c
duynnd <>
2021-05-21 19:13:45 +0700

    intergrate api

--- pages/thue-nha-thau.vue
+++ pages/thue-nha-thau.vue
@@ -13,55 +13,70 @@
                             </div>
                             <div class="line"></div>
                             <div class="list">
-                                <div class="d-flex item-title f-14">
+                                <div class="d-flex item-title f-14 cursor-pointer"
+                                    @click="getItemSearch({id:1},4)"
+                                    :class="activeParent == 1 ? 'fw-600 text-main' :'' "
+                                >
                                     <img src="@/assets/svg/homeproject.svg" alt="">
-                                    <span>Thi công xây dựng</span>
+                                    <span>{{objCategory[0].label}}</span>
                                 </div>
                                 <div class="list-item f-13">
                                     <ul>
-                                        <li>Thi công phần thô</li>
-                                        <li>Thi công hoàn thiện</li>
-                                        <li>Sửa chữa cải tạo</li>
-                                        <li>Thi công trọn gọi</li>
-                                        <li>Thi công nhà tiền chế</li>
-                                        <li>Thi công toà nhà</li>
-                                        <li>Hạng mục thi công khác</li>
+                                        <li @click="getItemSearch(item,0)"
+                                            v-for="(item) in objCategory[0].children"
+                                            :key="item.id"
+                                            :class="item.active ? 'fw-600 text-main' :'' "
+                                            class="cursor-pointer"
+                                        >
+                                            {{item.label}}
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="list">
-                                <div class="d-flex item-title f-14">
+                                <div class="d-flex item-title f-14 cursor-pointer"
+                                    @click="getItemSearch({id:2},4)"
+                                    :class="activeParent == 2 ? 'fw-600 text-main' :'' "
+                                >
                                     <img src="@/assets/svg/sofa.svg" alt="">
-                                    <span>Trang trí nội thất</span>
+                                     <span>{{objCategory[1].label}}</span>
                                 </div>
                                 <div class="list-item f-13">
                                     <ul>
-                                        <li>Nội thất căn hộ/nhà phố</li>
-                                        <li>Nội thất biệt thự</li>
-                                        <li>Nội thất văn phòng
-                                        <li>Nội thất nhà hàng</li>
-                                        <li>Nội thất quan bar/karaoke</li>
-                                        <li>Hạng mục nội thất khác</li>
+                                        <li @click="getItemSearch(item,1)"
+                                            v-for="(item) in objCategory[1].children"
+                                            :key="item.id"
+                                            :class="item.active ? 'fw-600 text-main' :'' "
+                                            class="cursor-pointer"
+                                        >
+                                            {{item.label}}
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
                             <div class="list">
-                                <div class="d-flex item-title f-14">
+                                <div class="d-flex item-title f-14 cursor-pointer"
+                                    @click="getItemSearch({id:3},4)"
+                                    :class="activeParent == 3 ? 'fw-600 text-main' :'' "
+                                >
                                     <img src="@/assets/svg/pen.svg" alt="">
-                                    <span>Thiết kế 2D/3D</span>
+                                    <span>{{objCategory[2].label}}</span>
                                 </div>
                                 <div class="list-item f-13">
                                     <ul>
-                                        <li>Thiết kế kiến trúc</li>
-                                        <li>Thiết kế nội thất</li>
-                                        <li>Bản vẽ kỹ thuật</li>
-                                        <li>Thiết kế kết cấu</li>
-                                        <li>Lập hồ sơ dự toán/hoàn công</li>
-                                        <li>Hạng mục thiết kế khác</li>
+                                         <li @click="getItemSearch(item,2)"
+                                            v-for="(item) in objCategory[2].children"
+                                            :key="item.id"
+                                            :class="item.active ? 'fw-600 text-main' :'' "
+                                            class="cursor-pointer"
+                                        >
+                                            {{item.label}}
+                                        </li>
                                     </ul>
                                 </div>
                             </div>
                         </div>
+
                         <div class="left">
                             <div class="top">
                                 <div class="title f-16">
@@ -96,36 +111,51 @@
                             <!-- search -->
                             <div class="top">
                                 <div class="search-top">
-                                    <input type="text" class="f-12" placeholder="Tìm kiếm đơn vị thi công/thiết kế">
+                                    <input  v-model="objProject.s"  type="text" class="f-12" placeholder="Tìm kiếm đơn vị thi công/thiết kế">
                                 </div>
-                                <div class="button">Tìm kiếm</div>
+                                <treeselect
+                                        class="search-center"
+                                        :options="optionsProvince"
+                                        :value="objProject.address"
+                                        v-model="objProject.address"
+                                        placeholder="Tỉnh thành"
+                                />
+                                <div class="button" @click="searchProject()">Tìm kiếm</div>
                             </div>
                             <div class="line"></div>
                             <!-- list off member -->
-                            <div class="center" v-for="(item, j) in 2" :key="j">
+                            <div class="center" v-for="item in listUsers" :key="item._id">
                                 <div class="boxinfo">
                                     <div class="row">
                                         <div class="col-lg-9 col-md-12 col-sm-12">
                                             <div class="introduce">
                                                 <div  class="cover-img"
                                                     :style="{
-                                                        'background-image': 'url(' + `${imgProfile}` + ')',
+                                                        'background-image': 'url(' + `${item.photo ? item.photo : noAvatar}` + ')',
                                                     }"
                                                 >
                                                 </div>
                                                 <div class="content">
                                                     <div class="title">
-                                                        <div class="sub f-14">Công ty cổ phần Epoint {{item}}</div>
+                                                        <div class="sub f-14">{{item.company ? item.company : item.name}}</div>
                                                         <div class="icon">
                                                             <img src="@/assets/svg/icon-location.svg" alt="">
-                                                            <span class="name f-11">Hồ Chí Minh</span>
+                                                            <span class="name f-11">{{item.province}}</span>
                                                         </div>
                                                     </div>
                                                     <div class="description f-13">Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất Econs được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thấtEcons được thành lập và phát triển suốt 8 năm qua theo mô hình dịch vụ trọn gói trong lĩnh vực thiết kế và hoàn thiện nội thất</div>
                                                     <div class="group">
-                                                        <div class="choose active">Nội thất căn hộ</div>
-                                                        <div class="choose">Thiết kế kiến trúc</div>
-                                                        <div class="choose">Thiết kế nội thất</div>
+                                                        <template v-if="item.category">
+                                                            <template v-for="cate in mapCategory(item.category)">
+                                                                <span
+                                                                    :key="cate.id"
+                                                                    class="px-8px badge py-2px text-12 custom-bage mr-8px mb-8px"
+                                                                    :class="getClassBageCategory(mapValueFromCategory(cate.id))"
+                                                                >
+                                                                    {{cate.label}}
+                                                                </span>
+                                                            </template>
+                                                        </template>
                                                     </div>
                                                 </div>
                                             </div>
@@ -155,6 +185,9 @@
                                 </div>
                                 <div class="line"></div>
                             </div>
+                            <template v-if="count>0">
+                                <Pagination :count="count" :perPage="5" @page="getPaging" />
+                            </template>
 
                         </div>
                     </div>
@@ -172,19 +205,127 @@
 <script>
 import ImgProfile from '@/assets/svg/longb.svg'
 import logoDuan from '@/assets/svg/logo-duan.svg'
-
+import IconBlueHouse from '@/assets/svg/icon-house-blue.svg'
+import IconDecordOrange from '@/assets/svg/icon-decord-orange.svg'
+import IconPenBlue from '@/assets/svg/icon-pen-green.svg'
+import NoAvatar from '@/assets/img/no-avatar.png'
 export default {
     data(){
         return{
             imgProfile: ImgProfile,
             logoDuan :logoDuan,
-            dataFake:[1,2,3,4,5,6,7]
+            noAvatar: NoAvatar,
+            dataFake:[1,2,3,4,5,6,7],
+            activeParent:0,
+            objCategory: this.getCategory(),
+            optionsProvince: this.getProvince(),
+            arrFilter:[],
+            objProject:{
+                address:null,
+                s:'',
+                limit:10,
+                page:1
+            },
+            listUsers:[],
+            count:0
         }
     },
     mounted(){
-
+        this.getPaging({limit:5, page:1})
     },
     methods:{
+        searchProject(){
+            this.getPaging({limit:10, page:1})
+        },
+        getItemSearch(item,idxArray){
+            var isRemove = false
+            //parent
+            if(idxArray == 4){
+                if(this.activeParent == item.id){
+                    this.activeParent = 0
+                    isRemove = true
+                }
+                else{
+                    this.activeParent = item.id
+                }
+                var arrTmp = JSON.parse(JSON.stringify(this.objCategory))
+                arrTmp.forEach(arr1 => {
+                    arr1.children.forEach(child => {
+                        child.active = false
+                    })
+                })
+                this.objCategory = arrTmp
+            }
+
+            //children
+            else{
+                this.activeParent = 0
+                var arrTmp = JSON.parse(JSON.stringify(this.objCategory))
+                arrTmp.forEach(arr1 => {
+                    arr1.children.forEach(child => {
+                        child.active = false
+                        if(item.active){
+                            isRemove = true
+                            return
+                        }
+                        if(child.id == item.id){
+                            child.active = true
+                        }
+                    })
+                })
+                this.objCategory = arrTmp
+            }
+            if(this.arrFilter.length){
+                this.arrFilter.splice(0,1)
+            }
+            if(isRemove){
+                this.arrFilter = []
+                this.getPaging({limit:10, page:1})
+                return
+            }
+
+            this.arrFilter.push(item)
+            this.getPaging({limit:10, page:1})
+        },
+        async getPaging(pageObj){
+            this.loader()
+            try{
+                this.objProject.category = []
+                this.arrFilter.forEach(item => {
+                    this.objProject.category.push(item.id)
+                })
+                let res = await this.$post('public/users ',{...this.objProject, ...pageObj})
+                this.listUsers = res.data.users
+                this.count = res.data.count
+                window.scrollTo(0,0)
+                this.loader(0)
+            }
+            catch(err){
+                this.loader(0)
+            }
+        },
+        getIconImg(value){
+            if(value == 1){
+                return this.iconBlueHouse
+            }
+            else if(value == 2){
+                return this.iconDecord
+            }
+            else{
+                return this.iconPenBlue
+            }
+        },
+        getIconImg(value){
+            if(value == 1){
+                return this.iconBlueHouse
+            }
+            else if(value == 2){
+                return this.iconDecord
+            }
+            else{
+                return this.iconPenBlue
+            }
+        },
         openLienHe(){
             this.$refs.refLienHe.show();
         },
