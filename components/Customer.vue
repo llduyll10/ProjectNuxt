1665e2ec076fea783ad3e57d748d2a7358a577b9
Alex Pham <phamyenbk@gmail.com>
2021-05-27 10:40:14 +0700

    update

--- components/Customer.vue
+++ components/Customer.vue
@@ -31,19 +31,13 @@
                 </template>
                 <p class="evaluate f-12 mb-0 mt-5px ml-4px">(<span>5</span>  đánh giá)</p>
             </div>
-            <p class="mb-0 f-13 mb-15px">Bạn sẽ tốn <span class="text-main fw-600">3 Tokens</span> khi liên hệ trực tiếp với khách đăng tin</p>
-            <div @click="openLienHe" class="btn-contact d-block mb-0 cursor-pointer">
-                <img src="@/assets/svg/icon-phone.svg" />
-                <span>Liên hệ trực tiếp</span>
-            </div>
-            <div class="btn-contact email">
-                <img src="@/assets/svg/icon-email.svg" />
-                <span>Gửi tin nhắn</span>
-            </div>
-            <div class="btn-contact">
-                <img src="@/assets/svg/icon-user-light.svg" />
-                <span>Cập nhật hồ sơ</span>
-            </div>
+            <template v-if="$auth.loggedIn">
+                <p class="mb-0 f-13 mb-15px">Bạn sẽ tốn <span class="text-main fw-600">3 Tokens</span> khi liên hệ trực tiếp với khách đăng tin</p>
+                <div @click="openLienHe" class="btn-contact d-block mb-0 cursor-pointer">
+                    <img src="@/assets/svg/icon-phone.svg" />
+                    <span>Liên hệ trực tiếp</span>
+                </div>
+            </template>
         </section>
         <PopupLienhe ref="refLienHe"/>
     </div>
