3b55bb6fa33a7bbcca9537eae094262e94d24386
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-06-08 11:30:13 +0700

    up

--- pages/chi-tiet.vue
+++ pages/chi-tiet.vue
@@ -10,13 +10,29 @@
                             <div class="row">
                                 <div class="col-lg-8">
                                     <div class="left">
-                                        <div class="slogan f-25">
-                                            Siêu điểm đến mới tại Nha Trang
+                                        <div class="detail">
+                                            <div class="slogan f-24 mb-8px">
+                                                Siêu điểm đến mới tại Nha Trang
+                                            </div>
+                                            <div class="sub-info f-15">
+                                                Ngay từ khi ra mắt, Vega City Nha Trang là "điểm sáng" của thị trường bất động sản nghỉ dưỡng bởi mô hình nghỉ dưỡng khép kín, cùng định hướng phát triển kinh tế đêm độc đáo, hứa hẹn đem đến một "thiên đường" giải trí, nghệ thuật sôi động bậc nhất, một kinh đô ánh sáng và lễ hội tưng bừng suốt 4 mùa. Do đó, những căn shophouse tại đây luôn có sức hút mạnh mẽ đối với các nhà đầu tư sành sỏi bởi tiềm năng du lịch và khai thác thương mại bất tận.
+                                            </div>
+                                            <div class="box-img mb-12px">
+                                                <img src="@/assets/svg/detailimage.svg" alt="">
+                                                <div class="sub-img f-14 mt-12px">
+                                                    Tầm view kép mang lại giá trị độc tôn cho Vip 18 shophouse Vega City Nha Trang.
+                                                </div>
+                                            </div>
+                                            <div class="sub-info f-15">
+                                                Ngay từ kh, Vega City Nha Trang là "điểm sáng" của thị trường bất động sản nghỉ dưỡng bởi mô hình nghỉ dưỡng khép kín, cùng định hướng phát triển kinh tế đêm độc đáo, hứa hẹn đem đến một "thiên đường" giải trí, nghệ thuật sôi động bậc nhất, một kinh đô ánh sáng và lễ hội tưng bừng suốt 4 mùa. Do đó, những căn shophouse tại đây luôn có sức hút mạnh mẽ đối với các nhà đầu tư sành sỏi bởi tiềm năng du lịch và khai thác thương mại bất tận.
+                                            </div>
+                                            <div class="sub-info f-15">
+                                                Ngay từ khi ra mắt, Vega City Nha Trang là "điểm sáng" của thị trường bất động sản nghỉ dưỡng bởi mô hình nghỉ dưỡng khép kín, cùng định hướng phát triển kinh tế đêm độc đáo, hứa hẹn đem đến một "thiên đường" giải trí, nghệ thuật sôi động bậc nhất, một kinh đô ánh sáng và lễ hội tưng bừng suốt 4 mùa. Do đó, những căn shophouse tại đây luôn có sức hút mạnh mẽ đối với các nhà đầu tư sành sỏi bởi tiềm năng du lịch và khai thác thương mại bất tận.
+                                            </div>
+                                            <div class="sub-info f-15">
+                                                Ngay từ khi ra mắt, Vega City Nha Trang là "điểm sáng" của thị trường bất động sản nghỉ dưỡng bởi mô hình nghỉ dưỡng khép kín, cùng định hướng phát triển kinh tế đêm độc đáo, hứa hẹn đem đến một "thiên đường" giải trí, nghệ thuật sôi động bậc nhất, một kinh đô ánh sáng và lễ hội tưng bừng suốt 4 mùa. Do đó, những căn shophouse tại đây luôn có sức hút mạnh mẽ đối với các nhà đầu tư sành sỏi bởi tiềm năng du lịch và khai thác thương mại bất tận.
+                                            </div>
                                         </div>
-                                        <div class="sub-info">
-                                            Ngay từ khi ra mắt, Vega City Nha Trang là "điểm sáng" của thị trường bất động sản nghỉ dưỡng bởi mô hình nghỉ dưỡng khép kín, cùng định hướng phát triển kinh tế đêm độc đáo, hứa hẹn đem đến một "thiên đường" giải trí, nghệ thuật sôi động bậc nhất, một kinh đô ánh sáng và lễ hội tưng bừng suốt 4 mùa. Do đó, những căn shophouse tại đây luôn có sức hút mạnh mẽ đối với các nhà đầu tư sành sỏi bởi tiềm năng du lịch và khai thác thương mại bất tận.
-                                        </div>
-                                        <img src="@/assets/svg/detailimage.svg" alt="">
                                     </div>
                                 </div>
                                 <div class="col-lg-4">
