6036836f04d4fa07add9e598af779010256b8bad
quocthinh <thinhvqps11484@fpt.edu.vn>
2021-06-09 12:24:52 +0700

    no message

---
+++ components/HomeAidNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <Nuxt-link :to="{path : '/hom-aid/gioi-thieu'}">Giới thiệu về HomAid</Nuxt-link>
+            </li>
+            <li>
+                <Nuxt-link :to="{path : '/hom-aid/cau-hoi-thuong-gap'}">Câu hỏi thường gặp</Nuxt-link>
+            </li>
+            <li>
+                <Nuxt-link :to="{path : '/hom-aid/tin-tuc-va-thi-truong'}">Tin tức & Thị trường</Nuxt-link>
+            </li>
+            <li>
+                <Nuxt-link :to="{path : '/hom-aid/co-hoi-nghe-nghiep'}">Cơ hội nghề nghiệp</Nuxt-link>
+            </li>
+            <li>
+                <Nuxt-link :to="{path : '/hom-aid/lien-he'}">Liên hệ</Nuxt-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    a.nuxt-link-exact-active {
+    color: rgba(25, 138, 198, 1) !important;
+    border-bottom : 1px solid rgba(25, 138, 198, 1);
+    }
+</style>
---
+++ pages/hom-aid/cau-hoi-thuong-gap.vue
@@ -0,0 +1,49 @@
+<template>
+    <div >
+        <Header />
+        <BannerInfo />
+        <section id="homeAid" class="pt-45px pb-50px">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="box">
+                            <div class="top">
+                                <HomeAidNav />
+                            </div>
+                            <div class="center">
+                                day la cau hoi thuong gap
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        <Footer />
+    </div>
+
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    a.nuxt-link-exact-active {
+        font-weight: bold;
+        color: #00c58e;
+    }
+</style>
