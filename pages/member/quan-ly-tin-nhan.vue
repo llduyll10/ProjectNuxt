9af0c28563223c12de9fa6ff85468404d8d40b47
duynnd <>
2021-06-14 14:52:16 +0700

    up

--- pages/member/quan-ly-tin-nhan.vue
+++ pages/member/quan-ly-tin-nhan.vue
@@ -13,17 +13,18 @@
                                         <div class="button">Tìm kiếm</div>
                                     </div>
                                     <div class="general d-flex">
-                                        <div class="item active f-13">
+                                        <div @click="typeRoom = 1" class="item cursor-pointer f-13" :class="typeRoom == 1 ? 'active' : ''" >
                                             Tất cả tin nhắn
                                         </div>
-                                        <div class="item f-13">
+                                        <div @click="typeRoom = 2" class="item cursor-pointer f-13" :class="typeRoom == 2 ? 'active' : '' " >
                                             Tin chưa đọc (<span>2</span>)
                                         </div>
-                                        <div class="item f-13">
-                                            Lưu trữ
+                                        <div @click="typeRoom = 3" class="item cursor-pointer f-13" :class="typeRoom == 3 ? 'active' : ''" >
+                                            Hỗ trợ (<span>2</span>)
                                         </div>
                                     </div>
                                 </div>
+                                <!-- List message -->
                                 <div v-if="listMess" class="wrapMessage">
                                     <div @click="getDetailMess(item)"
                                         class="messageItem d-flex  cursor-pointer"
@@ -36,9 +37,9 @@
                                         </div>
                                         <div class="content">
                                             <div class="top d-flex">
-                                                <div class="name f-12">
+                                                <!-- <div class="name f-12">
                                                     {{findUserChat(item.users).name}}
-                                                </div>
+                                                </div> -->
                                                 <div class="company f-12">
                                                     {{item.title}}
                                                 </div>
@@ -57,87 +58,94 @@
                                     </div>
                                 </div>
                             </div>
-                            <div v-if="currentRoom" class="contentWrap">
-                                <div  class="contentTop d-flex">
-                                    <div  class="title">{{currentRoom.title}}</div>
-                                    <div class="wrapDate">
-                                        <img src="@/assets/svg/point-black.svg" alt="">
-                                        <div class="date ml-20px f-12">Đăng ngày  <span>{{$moment(currentRoom.createdDate).format('DD/MM/YYYY')}}</span></div>
+                            <template v-if="typeRoom == 1 || typeRoom == 2">
+                                <div v-if="currentRoom" class="contentWrap">
+                                    <div  class="contentTop d-flex">
+                                        <div  class="title">{{currentRoom.title}}</div>
+                                        <div class="wrapDate">
+                                            <img src="@/assets/svg/point-black.svg" alt="">
+                                            <div class="date ml-20px f-12">Đăng ngày  <span>{{$moment(currentRoom.createdDate).format('DD/MM/YYYY')}}</span></div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="message mt-25px" v-if="listChatDetail" :class="arrFile.length ? 'fixHeight' :''">
-                                    <div v-for="(item , i ) in listChatDetail" :key="i+20">
-                                        <div class="dateMessage">{{$moment(item.createdDate).format('DD/MM/YYYY, hh:mm A')}}</div>
-                                        <!-- It me -->
-                                        <template v-if="$auth && $auth.user">
-                                            <div v-if="$auth.user._id != item.user._id"  class="send d-flex">
-                                                <img v-if="item.user.photo" :src="item.user.photo" alt="">
-                                                <div v-else class="not-avatar mr-15px">
-                                                    <span>{{item.user.name.slice(0,1).toUpperCase()}}</span>
-                                                </div>
-                                                <div class="d-flex flex-column">
-                                                    <div class="contentSend">
-                                                        {{item.message}}
-                                                        <br/>
-                                                        <template v-if="item.attachments">
-                                                            <template v-for="(file,idx) in item.attachments">
-                                                                <div :key="idx+101" class="mt-5px cursor-pointer" @click="downloadFile(file)">
-                                                                    <span  v-html="returnTypeFile(spliceURLFile(file,'--'))"></span>
-                                                                    <span class="f-11">{{spliceURLFile(file,'--')}}</span>
-                                                                </div>
+                                    <div class="message mt-25px" v-if="listChatDetail" :class="arrFile.length ? 'fixHeight' :''">
+                                        <div v-for="(item , i ) in listChatDetail" :key="i+20">
+                                            <div class="dateMessage">{{$moment(item.createdDate).format('DD/MM/YYYY, hh:mm A')}}</div>
+                                            <!-- It me -->
+                                            <template v-if="$auth && $auth.user">
+                                                <div v-if="$auth.user._id != item.user._id"  class="send d-flex">
+                                                    <img v-if="item.user.photo" :src="item.user.photo" alt="">
+                                                    <div v-else class="not-avatar mr-15px">
+                                                        <span>{{item.user.name.slice(0,1).toUpperCase()}}</span>
+                                                    </div>
+                                                    <div class="d-flex flex-column">
+                                                        <div class="contentSend">
+                                                            {{item.message}}
+                                                            <br/>
+                                                            <template v-if="item.attachments">
+                                                                <template v-for="(file,idx) in item.attachments">
+                                                                    <div :key="idx+101" class="mt-5px cursor-pointer" @click="downloadFile(file)">
+                                                                        <span  v-html="returnTypeFile(spliceURLFile(file,'--'))"></span>
+                                                                        <span class="f-11">{{spliceURLFile(file,'--')}}</span>
+                                                                    </div>
+                                                                </template>
                                                             </template>
-                                                        </template>
+                                                        </div>
                                                     </div>
                                                 </div>
-                                            </div>
-                                            <div v-else class="receive d-flex">
-                                                <div class="d-flex flex-column">
-                                                    <div class="contentReceive">
-                                                        {{item.message}}
-                                                        <br />
-                                                        <template v-if="item.attachments">
-                                                            <template v-for="(file,idx) in item.attachments">
-                                                                 <div :key="idx+11" class="mt-5px cursor-pointer"  @click="downloadFile(file)">
-                                                                    <span  v-html="returnTypeFile(spliceURLFile(file,'--'))"></span>
-                                                                    <span class="f-11">{{spliceURLFile(file,'--')}}</span>
-                                                                </div>
+                                                <div v-else class="receive d-flex">
+                                                    <div class="d-flex flex-column">
+                                                        <div class="contentReceive">
+                                                            {{item.message}}
+                                                            <br />
+                                                            <template v-if="item.attachments">
+                                                                <template v-for="(file,idx) in item.attachments">
+                                                                    <div :key="idx+11" class="mt-5px cursor-pointer"  @click="downloadFile(file)">
+                                                                        <span  v-html="returnTypeFile(spliceURLFile(file,'--'))"></span>
+                                                                        <span class="f-11">{{spliceURLFile(file,'--')}}</span>
+                                                                    </div>
+                                                                </template>
                                                             </template>
-                                                        </template>
+                                                        </div>
+                                                    </div>
+                                                    <img v-if="item.user.photo" :src="item.user.photo" alt="">
+                                                    <div v-else class="not-avatar ml-15px">
+                                                        <span>{{item.user.name.slice(0,1).toUpperCase()}}</span>
                                                     </div>
                                                 </div>
-                                                <img v-if="item.user.photo" :src="item.user.photo" alt="">
-                                                <div v-else class="not-avatar ml-15px">
-                                                    <span>{{item.user.name.slice(0,1).toUpperCase()}}</span>
-                                                </div>
-                                            </div>
-                                        </template>
+                                            </template>
+
+                                        </div>
+                                    </div>
+                                    <form @submit.prevent="sendMessage" class="wrapFill d-flex">
+                                        <input v-model="objMessage.message" type="text" placeholder="Soạn tin nhắn...">
+                                        <img src="@/assets/svg/happy.svg" alt="">
+                                        <!-- <img src="@/assets/svg/file.svg" alt=""> -->
+                                        <InputFile :accept="accepFile" @input="getFile" :isChat="true" :multiple="true" :label="'Thêm tài liệu'"/>
+
+                                        <button  class="sending" type="submit">Gửi tin nhắn</button>
+                                    </form>
+                                    <div  v-if="arrFile.length">
+                                        <div  class="listFile">
+                                            <template v-for="(item,idx) in arrFile">
+                                                    <p :key="idx" class="f-11 text-main mr-10px item-file">
+                                                        <span class="f-11" v-html="returnTypeFile(item.name)"></span>
+                                                        {{item.name}}
+                                                        <span class="cursor-pointer ml-5px" @click="clearFile(item)">
+                                                            <i class="fas fa-times text-red"></i>
+                                                        </span>
+                                                    </p>
+
+                                            </template>
+                                        </div>
 
                                     </div>
                                 </div>
-                                <form @submit.prevent="sendMessage" class="wrapFill d-flex">
-                                    <input v-model="objMessage.message" type="text" placeholder="Soạn tin nhắn...">
-                                    <img src="@/assets/svg/happy.svg" alt="">
-                                    <!-- <img src="@/assets/svg/file.svg" alt=""> -->
-                                    <InputFile :accept="accepFile" @input="getFile" :isChat="true" :multiple="true" :label="'Thêm tài liệu'"/>
-
-                                    <button  class="sending" type="submit">Gửi tin nhắn</button>
-                                </form>
-                                <div  v-if="arrFile.length">
-                                    <div  class="listFile">
-                                        <template v-for="(item,idx) in arrFile">
-                                                <p :key="idx" class="f-11 text-main mr-10px item-file">
-                                                    <span class="f-11" v-html="returnTypeFile(item.name)"></span>
-                                                    {{item.name}}
-                                                    <span class="cursor-pointer ml-5px" @click="clearFile(item)">
-                                                        <i class="fas fa-times text-red"></i>
-                                                    </span>
-                                                </p>
-
-                                        </template>
-                                    </div>
-
+                            </template>
+                            <template v-else>
+                                <div class="contentWrap">
+                                    <p>dbc</p>
                                 </div>
-                            </div>
+                            </template>
                         </div>
                     </div>
                 </div>
@@ -164,7 +172,8 @@ export default {
             currentRoom:null,
             listMess:null,
             listChatDetail:null,
-            arrFile:[]
+            arrFile:[],
+            typeRoom:1, //1,2: chat; 3:support
         }
     },
     mounted(){
