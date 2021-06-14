2d7040a729c53df32a05eca5c80d4491a34bf45c
duynnd <>
2021-06-14 17:19:56 +0700

    up

--- pages/member/quan-ly-tin-nhan.vue
+++ pages/member/quan-ly-tin-nhan.vue
@@ -147,7 +147,16 @@
                                 <div v-if="currentRoom" class="contentWrap">
                                     <!-- top -->
                                     <div  class="contentTop d-flex">
-                                        <div  class="title titleSupport">Phạm Yên vừa cập nhật thông tin dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 1" class="title titleSupport">Phạm Yên vừa gửi tin nhắn cho bạn</div>
+                                        <div v-if="typeSupport == 2" class="title titleSupport">Phạm Yên vừa cập nhật thông tin dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 3" class="title titleSupport">Phạm Yên mời bạn khảo sát dự án  <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 4" class="title titleSupport">Phạm Yên yêu cầu thương lượng dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 5" class="title titleSupport">Phạm Yên chọn bạn thi công dự án  <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 6" class="title titleSupport">Phạm Yên xác nhận thanh toán cho dự án  <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 7" class="title titleSupport">Phạm Yên vừa đánh giá chất lượng thi công dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+                                        <div v-if="typeSupport == 8" class="title titleSupport">Phạm Yên vừa kết thúc dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa </span></div>
+
+
                                         <div class="wrapDate">
                                             <img src="@/assets/svg/point-black.svg" alt="">
                                             <div class="date ml-20px f-12">Đăng ngày  <span>{{$moment(currentRoom.createdDate).format('DD/MM/YYYY')}}</span></div>
@@ -155,15 +164,101 @@
                                     </div>
                                     <!-- middle -->
                                     <div class="message isSuuport mt-25px px-25px" v-if="listChatDetail">
-                                        <span class="f-13 main-black">Chào bạn,</span> <br>
-                                        <span class="f-13 main-black">
-                                            Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa cập nhật thông tin dự án <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
-                                        </span>
-                                        <div class="btn-project">
-                                            <span>Xem dự án</span>
-                                        </div>
-                                        <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
-                                        <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        <template v-if="typeSupport == 1">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa gửi tin nhắn cho bạn về dự án <span class="text-main"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span> Bạn sẽ tốn <span class="text-main"> 5 Tokens </span> nếu mở tin nhắn. <span class="main-black font-weight-bold"> Lưu ý</span>, đây là chi phí thanh toán một lần duy nhất khi lần đầu bạn làm việc với khách hàng.
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem tin nhắn</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 2">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa cập nhật thông tin dự án <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem dự án</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 3">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span>  mời bạn tham gia khảo sát và cập nhật báo giá dự án<span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem yêu cầu khảo sát</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 4">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa gửi yêu cầu thượng lượng cho dự án <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem yêu cầu thương lượng</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 5">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa chọn bạn làm đơn vị thi công cho dự án <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem dự án</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+
+                                        <template v-if="typeSupport == 6">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa xác nhận đã thanh toán theo yêu cầu Thanh Toán Đợt 1 cho dự án  <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 7">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa đánh giá chất lượng thi công theo báo cáo tiến độ thi công Tuần 1 cho dự án   <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem đánh giá</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
+                                        <template v-if="typeSupport == 8">
+                                            <span class="f-13 main-black">Chào bạn,</span> <br>
+                                            <span class="f-13 main-black">
+                                                Khách hàng <span class="f-13 text-main">Phạm Yên</span> vừa kết thúc và hoàn tất đánh giá chất lượng thi công cho dự án <span class="f-13 text-main font-weight-bold"> Tìm đơn vị trang trí nội thất căn hộ Hà Đô Centrosa.</span>
+                                            </span>
+                                            <div class="btn-project">
+                                                <span>Xem đánh giá</span>
+                                            </div>
+                                            <span class="f-13 main-black">Chân thành cám ơn, </span> <br>
+                                            <span class="f-13 main-black"> Đội ngũ <span class="text-main">HomAid</span>,</span>
+                                        </template>
+
                                     </div>
                                     <!-- bottom -->
                                     <div class="content-bottom">
@@ -213,7 +308,8 @@ export default {
             listMess:null,
             listChatDetail:null,
             arrFile:[],
-            typeRoom:1, //1,2: chat; 3:support
+            typeRoom:1, //1,2: chat; 3:support,
+            typeSupport:4
         }
     },
     mounted(){
