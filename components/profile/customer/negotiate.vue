c3361372677c7ce97470ecc608ec4f06907a16bc
duynnd <>
2021-06-28 11:29:35 +0700

    up

--- components/profile/customer/negotiate.vue
+++ components/profile/customer/negotiate.vue
@@ -44,7 +44,7 @@
                     <td class="status ">
                         <template v-if="item.step == 0 ||item.step == 1 || item.step == 2 || item.step == 3">
 
-                                <b-dropdown v-if="item.step == 3" id="dropdown-duedate" variant="link" toggle-class="text-decoration-none" class="custom-infor pb-5px" no-caret>
+                                <b-dropdown v-if="item.step == 2" id="dropdown-duedate" variant="link" toggle-class="text-decoration-none" class="custom-infor pb-5px" no-caret>
                                     <template #button-content>
                                         <div class="d-flex">
                                             <div class="cover-infor">
@@ -55,7 +55,7 @@
                                             </div>
                                         </div>
                                     </template>
-                                    <b-dropdown-item @click="destroyDeal(item)" class="f-12" v-if="item.deal.length">
+                                    <b-dropdown-item @click="destroyDeal(item)" class="f-12">
                                         Huỷ thương lượng
                                     </b-dropdown-item>
                                 </b-dropdown>
