6b0ef3b095a71abac200034b8f2baefc53f66ba6
duynnd <>
2021-07-02 13:38:06 +0700

    update

--- pages/hom-aid/gioi-thieu.vue
+++ pages/hom-aid/gioi-thieu.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="center homeAidIntro">
+        <!-- 1 -->
         <div class="row">
             <div class="col-12">
                 <div class="box-title">
@@ -7,37 +8,179 @@
                     <img src="@/assets/svg/homeaidtext.svg" class="img-text" alt="">
                 </div>
                 <div class="box-center">
-                    <div class="text">Chào mừng bạn đã đến với  <span>Homaid.vn</span>, nền tảng kết nối chủ nhà và nhà thầu tại Việt Nam. Chúng tôi mong muốn cùng đồng hành với bạn trên từng dự án</div>
-                    <div class="heading f-17 mt-20px">
-                            <span>Hom</span><span class="aid">Aid</span> kết nối bạn thông qua mô hình đấu thầu trực tuyến
-                    </div>
-                    <div class="text mt-20px">
-                        Nếu bạn xây nhà lần đầu tiên và đang do dự không biết bắt đầu từ đâu, hãy đăng yêu cầu trên nền tảng của chúng tôi và nhận ngay tư vấn giải pháp và kinh phí dự trù từ các đơn vị thi công chuyên nghiệp đã được sàng lọc cẩn thận từ đội ngũ <span class="home">Hom</span><span class="aid">Aid</span>. Nếu như bạn là đơn vị thi công, công ty nội thất hay kiến trúc sư tự do, hãy đăng ký, hoàn thiện hồ sơ năng lực và kết nối ngay với nhiều khách hàng tiềm năng.
-                    </div>
-                    <div class="heading f-17 mt-30px">
-                            <span>Hom</span><span class="aid">Aid</span> giúp quản lý quá trình thi công nhẹ nhàng hơn
-                    </div>
-                    <ul class="mt-20px mb-30px">
-                        <li>
-                            <span>CHỦ ĐẦU TƯ</span> - Đăng tải công việc hoàn toàn miễn phí, yêu cầu khảo sát, thương lượng trực tiếp với nhà thầu trên nền tảng <span class="home">Hom</span><span class="aid">Aid</span>; nhận báo cáo thi công hàng tuần, theo dõi tiến độ và nhận đề nghị thanh toán trực tiếp từ nhà thầu.
-                        </li>
-                        <li>
-                            <span>ĐƠN VỊ THI CÔNG</span> - Lưu trữ hồ sơ thi công, thương lượng trực tiếp với chủ nhà, tạo báo cáo thi công & gửi đề nghị thanh toán trực tiếp trên <span class="home">Hom</span><span class="aid">Aid</span>
-                        </li>
-                    </ul>
-                    <div class="text">
-                        Với những tiêu chí trên, <span class="home">Hom</span><span class="aid">Aid</span> mong muốn tạo ra một môi trường làm việc thân thiện, an toàn, và đảm bảo công bằng cho cả chủ đầu tư & đơn vị thi công. Hãy đồng hành và cùng trải nghiệm các tiện ích tuyệt vời của chúng tôi ngay hôm nay.
-                    </div>
-                    <div class="heading-end f-15 mt-24px">Đội Ngũ <span class="home">Hom</span><span class="aid">Aid</span></div>
-                </div>
-                <div class="bottom d-flex">
-                        <div class="button d-flex">
-                            <div class="post f-20 mr-25px">ĐĂNG DỰ ÁN NGAY</div>
-                            <div class="report f-20">ĐĂNG KÝ BÁO GIÁ</div>
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-left">
+                            <div class="cover-img">
+                                <img src="@/assets/svg/homeaid-intro-1.svg" />
+                            </div>
+                            <div class="content">
+                                <p class="title">
+                                    <span class="text-main">Hom</span>Aid kết nối chủ nhà và nhà thầu thông qua mô <br>
+                                    <span>hình đấu thầu trực tuyến</span>
+                                </p>
+                                <p class="describe">
+                                    Nếu bạn xây nhà lần đầu tiên và đang do dự không biết bắt đầu từ đâu, hãy đăng yêu cầu trên nền tảng của chúng tôi và nhận ngay tư vấn giải pháp và kinh phí dự trù từ các đơn vị thi công chuyên nghiệp đã được sàng lọc cẩn thận từ đội ngũ <span class="text-main fw-600">Hom</span><span class="fw-600">Aid</span>. Nếu như bạn là <span class="fw-600">đơn vị thi công</span>, <span class="fw-600">công ty nội thất</span> hay <span class="fw-600">kiến trúc sư tự do</span>, hãy đăng ký, hoàn thiện hồ sơ <br>
+                                    <span class="end-describe">năng lực và kết nối ngay với nhiều khách hàng tiềm năng.</span>
+                                </p>
+                            </div>
+                            <div class="footer">
+                                <div class="btn-main">
+                                   <span>ĐĂNG DỰ ÁN NGAY</span>
+                                </div>
+                            </div>
                         </div>
-                    <img src="@/assets/svg/introHomeAidimg.svg"  alt="">
+                        <div class="col-12 col-md-6 col-right">
+                             <div class="cover-img">
+                                <img src="@/assets/svg/homeaid-intro-2.svg" />
+                            </div>
+
+                            <div class="content">
+                                <p class="title">
+                                    Nỗi lo của bạn & giải pháp của <span class="text-main fw-600">Hom</span><span class="fw-600">Aid</span>
+                                </p>
+
+                                <div class="group">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-price-tag.svg" />
+                                    </div>
+                                    <div class="value head">
+                                        Chi phí thi công không rõ ràng
+                                    </div>
+                                </div>
+                                <div class="group mb-20px">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-check.svg" />
+                                    </div>
+                                    <div class="value">
+                                        Với mô hình đấu giá trực tuyến, bạn sẽ nhận được giải pháp & báo giá cạnh tranh từ 3 nhà thầu
+                                    </div>
+                                </div>
+
+                                 <div class="group">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-quotation.svg" />
+                                    </div>
+                                    <div class="value head">
+                                        Tiến độ & chất lượng thi công
+                                    </div>
+                                </div>
+                                 <div class="group mb-20px">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-check.svg" />
+                                    </div>
+                                    <div class="value">
+                                        Tại thời điểm giao thầu và trong quá trình thi công, hệ thống sẽ yêu cầu nhà thầu báo cáo tiến độ bằng hình ảnh cho bạn
+                                    </div>
+                                </div>
+
+                                <div class="group">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-conversation.svg" />
+                                    </div>
+                                    <div class="value head">
+                                       Thanh toán không an toàn
+                                    </div>
+                                </div>
+                                <div class="group">
+                                    <div class="key">
+                                        <img class="fix" src="@/assets/svg/intro-check.svg" />
+                                    </div>
+                                    <div class="value">
+                                       Tuỳ theo yêu cầu của bạn, <span class="text-main fw-600">Hom</span><span class="fw-600">Aid</span> hỗ trợ yêu cầu đơn vị thi công ký quỹ với ngân hàng và cung cấp bảo lãnh thanh toán
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="footer">
+                                <div class="btn-main yellow">
+                                   <span>ĐĂNG KÝ BÁO GIÁ</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+        </div>
+        <!-- 2 -->
+        <div class="row description">
+            <p class="title">Giải pháp cho các công ty xây dựng, thi công nội thất và kiến trúc sư tự do </p>
+            <div class="group">
+                <div class="key">
+                    <img class="fix" src="@/assets/svg/intro-check.svg" />
+                </div>
+                <div class="value">
+                    Với mô hình đấu giá trực tuyến, bạn sẽ nhận được giải pháp & báo giá cạnh tranh từ 3 nhà thầu
+                </div>
+            </div>
+            <div class="group">
+                <div class="key">
+                    <img class="fix" src="@/assets/svg/intro-check.svg" />
+                </div>
+                <div class="value">
+                   Doanh nghiệp tư vấn trực tiếp online qua nền tảng của <span class="text-main fw-600">Hom</span><span class="fw-600">Aid</span>
+                </div>
+            </div>
+            <div class="group">
+                <div class="key">
+                    <img class="fix" src="@/assets/svg/intro-check.svg" />
+                </div>
+                <div class="value">
+                    Hỗ trợ quản lý trong quá trình thi công, lập báo cáo tuần/tháng, tạo yêu cầu thanh toán theo tiến độ...
+                </div>
+            </div>
+            <div class="group">
+                <div class="key">
+                    <img class="fix" src="@/assets/svg/intro-check.svg" />
+                </div>
+                <div class="value">
+                    Và nhiều dịch vụ khác hỗ trợ cho doanh nghiệp
+                </div>
+            </div>
+        </div>
+        <!-- br -->
+        <div class="br my-25px"></div>
+        <!-- 3 -->
+        <div class="row advisory">
+            <p class="title">
+                Nhận tư vấn miễn phí
+            </p>
+            <form @submit.prevent="sendAdvisory" class="col-12">
+                <div class="form-group row">
+                    <label class="col-12 pl-0">
+                        Họ và Tên
+                    </label>
+                    <input class="form-control col-12" placeholder="Họ và Tên" v-model="objAdvisory.name" />
+                </div>
+                 <div class="row">
+                    <div class="col-6">
+                        <div class="form-group row pr-15px">
+                            <label class="col-12 pl-0">
+                                Số điện thoại
+                            </label>
+                            <input class="form-control col-12" placeholder="Số điện thoại"  v-model="objAdvisory.phone"/>
+                        </div>
+                    </div>
+                     <div class="col-6">
+                        <div class="form-group row pl-15px">
+                            <label class="col-12 pl-0">
+                                Email
+                            </label>
+                            <input class="form-control col-12"  placeholder="Email"  v-model="objAdvisory.email" />
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row mb-30px">
+                    <label class="col-12 pl-0">
+                       Lời nhắn
+                    </label>
+                    <textarea class="form-control col-12" placeholder="Lời nhắn"  rows="5" v-model="objAdvisory.message" ></textarea>
+                </div>
+
+                <div class="row footer-form">
+                    <button type="submit" class="btn-form">
+                        <span>GỬI THÔNG TIN</span>
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -47,14 +190,18 @@
 export default {
     data(){
         return{
+            objAdvisory:{
 
+            }
         }
     },
     mounted(){
 
     },
     methods:{
-
+        sendAdvisory(){
+            console.log('aa',this.objAdvisory)
+        }
     }
 }
 </script>
